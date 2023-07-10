c19ba4a29c113c4c2fe2b3d3702c22a7745f7a6b
Leratho Biekman <biekman.l@hotmail.com>
2023-07-10 17:03:11 +0200

    three tabels for the database has been made. appliance, ingredients and recipes

--- seasoned_webapp/recipes/admin.py
+++ seasoned_webapp/recipes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Recipe
+from .models import Ingredient
+from .models import Appliance
 
-# Register your models here.
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Appliance)
--- seasoned_webapp/recipes/models.py
+++ seasoned_webapp/recipes/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=120)
+    kcal = models.IntegerField()
+    energy = models.IntegerField()
+    sugar = models.IntegerField()
+    salt = models.IntegerField()
+    carbohydrates = models.BigIntegerField()
+    protein = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Appliance(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField('recipe title', max_length=120)
+    description = models.TextField()
+    author = models.CharField(max_length=120)
+    date = models.DateTimeField()
+    nationality = models.CharField(max_length=120)
+    season = models.CharField(max_length=120)
+    difficulty = models.IntegerField()
+    ingredient = models.ManyToManyField(Ingredient, blank=True)
+    appliance = models.ManyToManyField(Appliance, blank=True)
+
+
+    def __str__(self):
+        return self.title
+    
+
+
+
+
