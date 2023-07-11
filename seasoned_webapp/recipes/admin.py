2a25a97c62bfe50cf1ff0f7d12b18d72c0f16353
Leratho Biekman <biekman.l@hotmail.com>
2023-07-11 23:26:50 +0200

    Update of the database setup. added tables wishes, nationalities and SeasonsHolidays. Changed the name of Appliance to Appliances

--- seasoned_webapp/recipes/admin.py
+++ seasoned_webapp/recipes/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Recipe
 from .models import Ingredient
-from .models import Appliance
+from .models import Appliances
+from .models import Nationalities
+from .models import Wishes
+from .models import SeasonsHolidays
 
-admin.site.register(Recipe)
+
 admin.site.register(Ingredient)
-admin.site.register(Appliance)
+admin.site.register(Appliances)
+admin.site.register(Nationalities)
+admin.site.register(Wishes)
+admin.site.register(SeasonsHolidays)
+admin.site.register(Recipe)
--- seasoned_webapp/recipes/models.py
+++ seasoned_webapp/recipes/models.py
@@ -2,23 +2,44 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=120)
-    kcal = models.IntegerField()
-    energy = models.IntegerField()
-    sugar = models.IntegerField()
-    salt = models.IntegerField()
-    carbohydrates = models.BigIntegerField()
-    protein = models.IntegerField()
+    kcal = models.IntegerField(default=-1)
+    energy = models.IntegerField(default=-1)
+    sugar = models.IntegerField(default=-1)
+    salt = models.IntegerField(default=-1)
+    carbohydrates = models.IntegerField(default=-1)
+    protein = models.IntegerField(default=-1)
 
     def __str__(self):
         return self.name
 
 
-class Appliance(models.Model):
+class Appliances(models.Model):
     name = models.CharField(max_length=120)
 
     def __str__(self):
         return self.name
 
+
+class Wishes(models.Model):
+    wish = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.wish
+    
+
+class Nationalities(models.Model):
+    nationality = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.nationality
+
+
+class SeasonsHolidays(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+ 
 
 class Recipe(models.Model):
     title = models.CharField('recipe title', max_length=120)
@@ -29,11 +50,17 @@ class Recipe(models.Model):
     season = models.CharField(max_length=120)
     difficulty = models.IntegerField()
     ingredient = models.ManyToManyField(Ingredient, blank=True)
-    appliance = models.ManyToManyField(Appliance, blank=True)
+    appliances = models.ManyToManyField(Appliances, blank=True)
+    wishes = models.ManyToManyField(Wishes, blank=True)
+    nationality = models.ManyToManyField(Nationalities, blank=True)
+    seasonsHolidays = models.ManyToManyField(SeasonsHolidays, blank=True)
 
 
     def __str__(self):
         return self.title
+
+
+
     
 
 
